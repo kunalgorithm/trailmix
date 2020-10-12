fc12f9e513575f90add9d4d8633ad4edb7eb0269
Kunal Shah <me@kunal.sh>
2020-10-11 23:40:17 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import { Col, Row, Divider } from "antd";
+import { Feed } from "../components/Feed";
+import { Profile } from "../components/Profile";
+import { CreateTweetForm } from "../components/CreateTweetForm";
+import Head from "next/head";
+export default () => (
+  <Row>
+    <Head>
+      <title>Fullstack Twitter Clone</title>
+    </Head>
+    <Col md={{ span: 6, offset: 2 }} xs={{ span: 12, offset: 2 }}>
+      <Profile />
+      <Divider />
+      <a href="https://github.com/kunalgorithm/fullstack-twitter">
+        View Source on Github
+      </a>
+    </Col>
+    <Col md={{ span: 10 }} xs={{ span: 20, offset: 2 }}>
+      <CreateTweetForm />
+      <Feed />
+    </Col>
+  </Row>
+);
